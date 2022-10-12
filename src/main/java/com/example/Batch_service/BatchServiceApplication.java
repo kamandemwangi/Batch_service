bbe1d9426e0849ffb4fb6b341bd86e0e4fad1f21
John Kamande <22johnmwangi@gmail.com>
2022-10-12 12:16:55 +0300

    Batch_service initial release

---
+++ src/main/java/com/example/Batch_service/BatchServiceApplication.java
@@ -0,0 +1,15 @@
+package com.example.Batch_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchServiceApplication {
+
+	public static void main(String[] args) throws Exception {
+
+		//System.exit(SpringApplication.exit(SpringApplication.run(BatchServiceApplication.class, args)));
+		SpringApplication.run(BatchServiceApplication.class, args);
+	}
+
+}
